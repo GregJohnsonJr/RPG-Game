9aea7057dea7fceb9813cda21f18adcc87644ff2
YoungKrug <gregjohn456@gmail.com>
2019-09-26 09:53:51 -0400

    Pushing AI scripts, and Navmesh stuff

--- RPG/My First RPG/Assets/Scripts/Ai/CityAI.cs
+++ RPG/My First RPG/Assets/Scripts/Ai/CityAI.cs
@@ -23,59 +23,94 @@ public class CityAI : MonoBehaviour
     public bool isAduilt;
     [Tooltip("Is the current npc a Merchant")]
     public bool isMerchant; // we can get more specific with the types later
-    public float runSpeed;
-    public float walkSpeed;
+    public float runSpeed = 16f;
+    public float walkSpeed = 8f;
     NavMeshAgent agent;
     public float rangeToExplore;
     Vector3 startPos;
+    Vector3 exploringPos;
     float dist;
     AIExtensions aiExtra;
+    System.Random rand;
+    bool isExploring;
     // Start is called before the first frame update
     void Start()
     {
         aiExtra = new AIExtensions();
         agent = gameObject.GetComponent<NavMeshAgent>();
+        rand = new System.Random(System.Environment.TickCount);
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         dist = Vector3.Distance(transform.position, startPos);
-        startPos = transform.position;
+        StateChangeMachine();
     }
     void StateChangeMachine()
     {
-        switch(states)
+        if (!isExploring)
         {
-            case States.WALKAROUND:
-                WalkAroundState();
-                break;
-            case States.LOOKAROUND:
-                LookAroundState();
-                break;
-            case States.RUNAROUND:
-                RunAroundState();
-                break;
-            case States.JUMP:
-                JumpState();
-                break;
-            case States.STAND:
-                StandState();
-                break;
+            int num = rand.Next(0, 1);
+            if (num == 0)
+            {
+                states = States.WALKAROUND;
+            }
+            else
+                states = States.RUNAROUND;
         }
+
+            switch (states)
+            {
+                case States.WALKAROUND:
+                    WalkAroundState();
+                    break;
+                case States.LOOKAROUND:
+                    LookAroundState();
+                    break;
+                case States.RUNAROUND:
+                    RunAroundState();
+                    break;
+                case States.JUMP:
+                    JumpState();
+                    break;
+                case States.STAND:
+                    StandState();
+                    break;
+            }
     }
     //NPCS are only allowed to stand and lookARound
+
     void WalkAroundState() //  This state will make the npc walk around its area
     {
-       if(dist < rangeToExplore)
-        {
+       if(dist < rangeToExplore && !isExploring)
+       {
             //Find random postion and move them to that position
             Vector3 rand = aiExtra.FindRandomPosition(startPos, rangeToExplore);
+            agent.destination = rand;
+            agent.speed = walkSpeed;
+            isExploring = true;
+       }
+       if(agent.remainingDistance <= 0.4f)
+        {
+            isExploring = false;
         }
     }
     void RunAroundState() // this state will make the npc run to random areas. If they are a kid they hae a higher change of running to different areas --Excludes NPCS
     {
-
+        if (dist < rangeToExplore && !isExploring)
+        {
+            //Find random postion and move them to that position
+            Vector3 rand = aiExtra.FindRandomPosition(startPos, rangeToExplore);
+            agent.destination = rand;
+            agent.speed = runSpeed;
+            isExploring = true;
+        }
+        if (agent.remainingDistance <= 0.4f)
+        {
+            isExploring = false;
+        }        
     }
     void LookAroundState() // This state will make the npc stop and look around its area --Merchants manly
     {
